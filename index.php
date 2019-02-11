56f01aa48c6c5972be10f4abab3e84ad9997fece
Materioprima <mateoburbano03@gmail.com>
2019-02-11 06:41:20 +0100

    Primera implementacion slider

--- index.php
+++ index.php
@@ -5,6 +5,15 @@
 </head>
 <body>
 	<?php require 'includes/menu.php'; ?>
+	<!--Contenido-->
+	<div id="slider">
+  		<a href="#" class="control_next">></a>
+  		<a href="#" class="control_prev"><</a>
+  		<ul>
+    		<li>SLIDE 1</li>
+    		<li><img class="imgSlide" src="https://areajugones.sport.es/wp-content/uploads/2018/07/Zoku-Owarimonogatari-new-key-1-e1531073837458-810x400.jpg"></li>
+    	</ul>
+    </div>
 	<?php require 'includes/metasfooter.php' ?>
 </body>
 </html>
